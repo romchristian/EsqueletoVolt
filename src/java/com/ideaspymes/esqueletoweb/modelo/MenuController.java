37bed25833e2cdfd8c41219a8ccc1be25f428b71
cromero <cromero@palermo.com.py>
2015-12-18 18:31:44 -0300

    Inicial

--- src/java/com/ideaspymes/esqueletoweb/modelo/MenuController.java
+++ src/java/com/ideaspymes/esqueletoweb/modelo/MenuController.java
@@ -11,6 +11,12 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import org.primefaces.component.menu.Menu;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuElement;
+import org.primefaces.model.menu.MenuModel;
 
 /**
  *
@@ -22,9 +28,37 @@ public class MenuController implements Serializable {
 
     private List<Modulo> modulos;
     private Modulo moduloActual;
+    private MenuModel menuModel;
+
+    public MenuModel getMenuModel() {
+        return menuModel;
+    }
+
+    public void setMenuModel(MenuModel menuModel) {
+        this.menuModel = menuModel;
+    }
+    
+        
 
     @PostConstruct
     public void init() {
+        menuModel = new DefaultMenuModel();
+        
+        DefaultSubMenu menu1 = new DefaultSubMenu("Sesiones","fa fa-money");
+        
+        menu1.addElement(new DefaultMenuItem("Su sesion", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        menu1.addElement(new DefaultMenuItem("Todas las sesiones", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        menu1.addElement(new DefaultMenuItem("Tickets", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        
+        menuModel.addElement(menu1);
+        
+        DefaultSubMenu menu2 = new DefaultSubMenu("Productos","fa fa-money");
+        
+        menu2.addElement(new DefaultMenuItem("Productos", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        menu2.addElement(new DefaultMenuItem("Categorias", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        menu2.addElement(new DefaultMenuItem("Unidades de Medida", "fa fa-money", "/main/puntoventa/home.xhtml"));
+        
+        menuModel.addElement(menu2);
         cargaModulos();
     }
 
@@ -49,7 +83,7 @@ public class MenuController implements Serializable {
 
     private void cargaModulos() {
         modulos = new ArrayList<>();
-        Modulo modulo1 = new Modulo("Punto de Venta", "icon-printer2 Opac80", "/EsqueletoVolt/main/puntoventa/home.xhtml", "Tickets, Facturas..");
+        Modulo modulo1 = new Modulo("Punto de Venta", "icon-printer2 Opac80 Fs22", "/EsqueletoVolt/main/puntoventa/home.xhtml", "Tickets, Facturas..");
         Submenu menuSesiones = new Submenu(modulo1, "Sesiones", "icon-select");
         MenuItem mItemMiSesion = new MenuItem(menuSesiones, "#", "Mi Sesión", "icon-uniE675");
         List<MenuItem> listaItems1 = new ArrayList<>();
@@ -71,10 +105,10 @@ public class MenuController implements Serializable {
         
         
         moduloActual = modulo1;
-        modulos.add(new Modulo("Contabilidad", "icon-printer2 Opac80", "/EsqueletoVolt/main/contabilidad/home.xhtml", "Cuentas, Asientos.."));
-        modulos.add(new Modulo("Compras", "icon-printer2 Opac80", "/EsqueletoVolt/main/compras/home.xhtml", "Órdenes de compras, etc."));
-        modulos.add(new Modulo("Ventas", "icon-printer2 Opac80", "/EsqueletoVolt/main/ventas/home.xhtml", "Presupuestos, Pedidos.."));
-        modulos.add(new Modulo("Stock", "icon-printer2 Opac80", "/EsqueletoVolt/main/stock/home.xhtml", "Stock, Movimientos.."));
+        modulos.add(new Modulo("Contabilidad", "icon-printer2 Opac80 Fs22", "/EsqueletoVolt/main/contabilidad/home.xhtml", "Cuentas, Asientos.."));
+        modulos.add(new Modulo("Compras", "icon-printer2 Opac80 Fs22", "/EsqueletoVolt/main/compras/home.xhtml", "Órdenes de compras, etc."));
+        modulos.add(new Modulo("Ventas", "icon-printer2 Opac80 Fs22", "/EsqueletoVolt/main/ventas/home.xhtml", "Presupuestos, Pedidos.."));
+        modulos.add(new Modulo("Stock", "icon-printer2 Opac80 Fs22", "/EsqueletoVolt/main/stock/home.xhtml", "Stock, Movimientos.."));
         modulos.add(modulo1);
     }
 
